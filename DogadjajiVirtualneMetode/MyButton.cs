2c90e68914b28a08f948826878f7a23cef890124
Marijo Horvat <Marijo.Horvat@vsite.hr>
2016-12-15 20:51:55 +0100

    igranje s gumbima

---
+++ DogadjajiVirtualneMetode/MyButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vsite.CSharp
+{
+    class MyButton : System.Windows.Forms.Button
+    {
+        protected override void OnClick(EventArgs e)
+        {
+            MessageBox.Show("Kliknuo si me!", "Botun obični");
+            base.OnClick(e);
+        }
+    }
+}
